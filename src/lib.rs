2a681c5c37b4912cd94e7255c6794162ffc5f8e7
philipahlberg <philipahlberg@gmail.com>
2020-01-01 14:57:59 +0100

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ use std::iter::Fuse;
 
 /// Create a lookahead iterator from `iterable`.
 pub fn lookahead<I>(iterable: I) -> Lookahead<I::IntoIter>
-    where I: IntoIterator {
+where
+    I: IntoIterator,
+{
     Lookahead {
         iter: iterable.into_iter().fuse(),
         queue: VecDeque::new(),
@@ -12,46 +14,45 @@ pub fn lookahead<I>(iterable: I) -> Lookahead<I::IntoIter>
 
 #[derive(Clone, Debug)]
 pub struct Lookahead<I: Iterator> {
-	iter: Fuse<I>,
-	queue: VecDeque<I::Item>,
+    iter: Fuse<I>,
+    queue: VecDeque<I::Item>,
 }
 
 impl<I: Iterator> Lookahead<I> {
     /// Return a reference to the item `n` iterations ahead without advancing the iterator.
-    /// 
+    ///
     /// When `n` is `0`, it is equivalent to [`Peekable::peek`].
     ///
     /// [`Peekable::peek`]: https://doc.rust-lang.org/std/iter/struct.Peekable.html#method.peek
-	pub fn lookahead(&mut self, n: usize) -> Option<&I::Item> {
+    pub fn lookahead(&mut self, n: usize) -> Option<&I::Item> {
         let enqueued = self.queue.len();
-		if n >= enqueued {
+        if n >= enqueued {
             let iter = &mut self.iter;
-			let items = iter.take(n - enqueued + 1);
-			self.queue.extend(items);
-		}
-		self.queue.get(n)
-	}
+            let items = iter.take(n - enqueued + 1);
+            self.queue.extend(items);
+        }
+        self.queue.get(n)
+    }
 }
 
 impl<I> Iterator for Lookahead<I>
-	where I: Iterator {
-	type Item = I::Item;
+where
+    I: Iterator,
+{
+    type Item = I::Item;
 
-	fn next(&mut self) -> Option<Self::Item> {
-		self.queue.pop_front()
-			.or_else(|| self.iter.next())
-	}
+    fn next(&mut self) -> Option<Self::Item> {
+        self.queue.pop_front().or_else(|| self.iter.next())
+    }
 
-	fn size_hint(&self) -> (usize, Option<usize>) {
-		let queued = self.queue.len();
-		let (lower, upper) = self.iter.size_hint();
-		(lower + queued, upper.map(|n| n + queued))
-	}
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let queued = self.queue.len();
+        let (lower, upper) = self.iter.size_hint();
+        (lower + queued, upper.map(|n| n + queued))
+    }
 }
 
-impl<I> ExactSizeIterator for Lookahead<I>
-    where I: ExactSizeIterator
-{}
+impl<I> ExactSizeIterator for Lookahead<I> where I: ExactSizeIterator {}
 
 #[cfg(test)]
 mod tests {
