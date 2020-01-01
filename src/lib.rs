d6e1627ea4a925f7a894415d9230540008d019ef
philipahlberg <philipahlberg@gmail.com>
2020-01-01 14:32:03 +0100

    Setup

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+use std::collections::VecDeque;
+use std::iter::Fuse;
+
+pub fn lookahead<I>(iterable: I) -> Lookahead<I::IntoIter>
+    where I: IntoIterator {
+    Lookahead {
+        iter: iterable.into_iter().fuse(),
+        queue: VecDeque::new(),
+    }
+}
+
+pub struct Lookahead<I: Iterator> {
+	iter: Fuse<I>,
+	queue: VecDeque<I::Item>,
+}
+
+impl<I: Iterator> Lookahead<I> {
+	pub fn lookahead(&mut self, n: usize) -> Option<&I::Item> {
+        let enqueued = self.queue.len();
+		if n >= enqueued {
+            let iter = &mut self.iter;
+			let items = iter.take(n - enqueued + 1);
+			self.queue.extend(items);
+		}
+		self.queue.get(n)
+	}
+}
+
+impl<I> Iterator for Lookahead<I>
+	where I: Iterator {
+	type Item = I::Item;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		self.queue.pop_front()
+			.or_else(|| self.iter.next())
+	}
+
+	fn size_hint(&self) -> (usize, Option<usize>) {
+		let queued = self.queue.len();
+		let (lower, upper) = self.iter.size_hint();
+		(lower + queued, upper.and_then(|n| upper.map(|m| n + m)))
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zero() {
+        let inner = [1, 2].into_iter();
+        let mut iter = lookahead(inner);
+        assert_eq!(iter.lookahead(0), Some(&&1));
+    }
+
+    #[test]
+    fn one() {
+        let inner = [1, 2].into_iter();
+        let mut iter = lookahead(inner);
+        assert_eq!(iter.lookahead(1), Some(&&2));
+    }
+
+    #[test]
+    fn two() {
+        let inner = [1, 2].into_iter();
+        let mut iter = lookahead(inner);
+        assert_eq!(iter.lookahead(2), None);
+    }
+
+    #[test]
+    fn next() {
+        let inner = [1, 2].into_iter();
+        let mut iter = lookahead(inner);
+        let _ = iter.next();
+        assert_eq!(iter.lookahead(0), Some(&&2));
+    }
+}
